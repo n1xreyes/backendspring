c8092bad3eef827c0a1d4ff34ad6b70242b0c662
anjello <n1xreyes@gmail.com>
2022-06-27 08:59:12 -0400

    some test code

--- src/main/java/com/angelo/bitcollect/backendspring/ComplexNumber.java
+++ src/main/java/com/angelo/bitcollect/backendspring/ComplexNumber.java
@@ -2,12 +2,36 @@ package com.angelo.bitcollect.backendspring;
 
 public class ComplexNumber {
 
-    double real;
-    double imaginary;
+    private double real;
+    private double imaginary;
 
     ComplexNumber(double real, double imaginary) {
         this.real = real;
         this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        double v = (this.real + real) + (this.imaginary + imaginary);
+    }
+
+    public void add(ComplexNumber complexNumber) {
+        add(complexNumber.getReal(), complexNumber.getImaginary());
+    }
+
+    public void subtract(double real, double imaginary) {
+        double v = (this.real - real) + (this.imaginary - imaginary);
+    }
+
+    public void subtract(ComplexNumber complexNumber) {
+        subtract(complexNumber.getReal(), complexNumber.getImaginary());
     }
 
 
